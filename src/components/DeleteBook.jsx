e6a6a3cc487086a55035aa9cef6fee75dc7711af
Sk Anarul <112712827+skanarul8002@users.noreply.github.com>
2024-05-26 21:04:02 +0530

    Update DeleteBook.jsx

--- src/components/DeleteBook.jsx
+++ src/components/DeleteBook.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-const REACT_APP_BASE_URL = process.env.REACT_APP_BASE_URL;
+const REACT_APP_BASE_URL = "https://bookstore-1ljq.onrender.com";
 const DeleteBook = () => {
   const navigate = useNavigate();
   const { id } = useParams();
